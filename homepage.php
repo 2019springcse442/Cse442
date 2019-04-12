53caa6bcd0e325ec802fdb2018dd526472d2460c
justizsb <bzheng2@buffalo.edu>
2019-04-11 21:56:10 -0400

    Homepage Navigation Bar Resizeable #56
    In this commit, #56 user story is completely fulfilled. It is under #40. 
    1) Homepage Navigation bar is updated.
    2) When the size of the browser is large enough to display all three subpages, all subpages will show on the right upper corner.
    2.1) When user click on one of them, they will be directed to the subpage they want to go.
    3) When the size of the browser is not large enough, the navigation bar will show a clickable hamburger menu.
    3.1) When user click on the hamburger menu, all three subpages display and they can click on the one they want to go.
    
    Further Implementation of Homepage:
    1) Resizeable search bar and search button
    2) Professor Database

--- homepage.php
+++ homepage.php
@@ -8,34 +8,37 @@
     <link rel="stylesheet" href="homepage.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <title> UBStalking'-' </title>
-    <h1>UBStalking'-'</h1>
 </head><!---->
 <body>
   <!-- here is the search bar-->
-  <div>
-  <form action="ProfessorProfileTemplate.php" method="post">
-    <input type = "text" id = "search-bar" placeholder = "Search for your professors!" value ="" name = "search"  maxlength ="50"
-    autocompleete = "off" onMousedown ="" onBlur = "" /><input type = "submit" id = "search-btn" name = "submit-search" value="Go!"/>
-  </form>
-  </div>
 
+  <!-- navigation bar -->
+  <header class="header">
+  <a href="" class="logo">UBStalking'-'</a>
+  <input class="menu-btn" type="checkbox" id="menu-btn" />
+  <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+  <ul class="menu">
+    <li><a href="OfficeNavigation.html">Office Navigation</a></li>
+    <li><a href="ScheduleMatching.html">Schedule Matching</a></li>
+    <li><a href="#">Report Bugs</a></li>
+  </ul>
+</header>
+   <!-- big logo -->
+   <div class="center-logo">
+     <h1 href="homepage.php" class="center-logo-text">UBStalking'-'</h1>
+     </div>
 
-  <nav>
-    <div class = "logo" href="homepage.html">
-      <h3> UBStalking'-' </h3>
+  <!-- search bar -->
+  <div class = "overall-search">
+        <div>
+        <form action="ProfessorProfileTemplate.php" method="post">
+          <input type = "text" id = "search-bar" placeholder = "Search for your professors!" value ="" name = "search"  maxlength ="50"
+          autocompleete = "off" onMousedown ="" onBlur = "" /><input type = "submit" id = "search-btn" name = "submit-search" value="Go!"/>
+        </form>
+        </div>
+      </div>
     </div>
-    <ul class = "nav-sub">
-      <li><a href="ScheduleMatching.html">Schedule Matching</a>
-      </li>
-      <li><a href="OfficeNavigation.html">Office Navigation</a>
-      </li>
-      <li><a href="#">Report Bugs</a>
-      </li>
-    </ul>
-  </nav>
+
 
 </body>
 </html>
-
-
-
