4bad99bfd3864b89bf06d328aa691363d2d13425
justizsb <bzheng2@buffalo.edu>
2019-04-27 21:40:00 -0400

    Fixed search bar in homepage
    In this commit, I fixed the search bar in homepage, so it is resizeable now!
    
    Further:
    1) Test by teammates
    2) Adjust if anything happened

--- homepage.php
+++ homepage.php
@@ -4,6 +4,7 @@
 
 <html>
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah|Laila" rel="stylesheet">
     <link rel="stylesheet" href="homepage.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
@@ -17,7 +18,7 @@
   <input class="menu-btn" type="checkbox" id="menu-btn" />
   <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
   <ul class="menu">
-    <li><a href="#">Report Bugs</a></li>
+    <li><a lang="en" href="mailto:ubstalking@gmail.com">Report Bugs</a></li>
     <li><a href="OfficeNavigation.html">Office Navigation</a></li>
     <li><a href="ScheduleMatching.php">Schedule Matching</a></li>
   </ul>
@@ -28,15 +29,12 @@
      </div>
 
   <!-- search bar -->
-  <div class = "overall-search">
-        <div>
-        <form action="ProfessorProfileTemplate.php" method="post">
-          <input type = "text" id = "search-bar" placeholder = "Search for your professors!" value ="" name = "search"  maxlength ="50"
-          autocompleete = "off" onMousedown ="" onBlur = "" /><input type = "submit" id = "search-btn" name = "submit-search" value="Go!"/>
-        </form>
-        </div>
-      </div>
-    </div>
+  <div>
+   <form action="ProfessorProfileTemplate.php" method="post">
+     <input type = "text" id = "search-bar" placeholder = "Search for your professors!" value ="" name = "search"  maxlength ="50"
+     autocompleete = "off" onMousedown ="" onBlur = "" /><input type = "submit" id = "search-btn" name = "submit-search" value="Go!"/>
+   </form>
+   </div>
 
 
 </body>
